db7fabfaa0ee16fc31db54def23cc494268aca6b
Srbislav Nešić <srbislav.nesic@fincore.com>
2024-04-14 04:08:55 +0200

    MAK

--- deck.go
+++ deck.go
@@ -4,10 +4,10 @@ package main
 
 const ( // preferans order
 	NoSuit      = iota
-	SpadeSuit   // ♠ pik
-	DiamondSuit // ♦ karo
-	HeartSuit   // ♥ srce
-	ClubSuit    // ♣ tref
+	SpadeSuit   // ♠ pik, лист
+	DiamondSuit // ♦ karo, баклава
+	HeartSuit   // ♥ srce, срце
+	ClubSuit    // ♣ tref, детелина
 )
 
 type (
