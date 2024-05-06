2294330e61ab46ea914416a138a6469737c90663
Srbislav Nešić <srbislav.nesic@fincore.com>
2024-05-06 14:11:42 +0200

    Histogram

--- rng/histogram.go
+++ rng/histogram.go
@@ -14,8 +14,7 @@ type Histogram struct {
 	Data       map[int]int
 	Min, Max   int
 	Mode, Peak int
-	x1, x2     float64
-	y1, y2     float64
+	ox, oy     float64
 	dx, dy, k  float
 	f          []func(x float64) float64
 	RNG        LCPRNG
@@ -27,28 +26,33 @@ func (h *Histogram) Reset(f ...func(x float64) float64) {
 	h.Data = map[int]int{}
 	h.f = f
 	h.Scale(1, 1)
-	h.Peak = 0
+	h.Mode, h.Peak = 0, 0
 }
 
-func (h *Histogram) Map(x1, x2, y1, y2 float64) {
-	h.x1, h.x2 = x1, x2
-	h.y1, h.y2 = y1, y2
-	h.dy = h.y2 - h.y1
-	h.dx = h.x2 - h.x1
-	h.k = h.dy / h.dx
+// # Define line (x1, y1)--(x2, y2)
+func (h *Histogram) Linear(x1, y1, x2, y2 float64) {
+	h.dy = y2 - y1
+	h.dx = x2 - x1
+	if h.dx == 0 || h.dy == 0 {
+		h.Scale(1, 1)
+	} else {
+		h.k = h.dy / h.dx
+		h.ox = x1
+		h.oy = y1
+	}
 }
 
-func (h *Histogram) Scale(p, q float64) {
-	h.Map(0, p, 0, q)
+func (h *Histogram) Scale(sx, sy float64) {
+	h.Linear(0, 0, sx, sy)
 }
 
 func (h *Histogram) Y(x float64) float64 {
-	y := (x-h.x1)*h.k + h.y1
+	y := (x-h.ox)*h.k + h.oy
 	return y
 }
 
 func (h *Histogram) X(y float64) float64 {
-	x := (y-h.y1)/h.k + h.x1
+	x := (y-h.oy)/h.k + h.ox
 	return x
 }
 
@@ -106,7 +110,7 @@ func (h *Histogram) Graph(width int, limit int, cumul bool, nozero ...bool) {
 	fmt.Println()
 	df := iif(cumul, "CDF", "PDF")
 	fmt.Printf("%s %s:   n = %d   [%v, %v]   μ = %.5f  σ = %.5f\n",
-		df, h.Title, h.Calc.Cnt, str(h.Calc.Min), str(h.Calc.Max), h.Calc.Avg, h.Calc.Dev)
+		h.Title, df, h.Calc.Cnt, str(h.Calc.Min), str(h.Calc.Max), h.Calc.Avg, h.Calc.Dev)
 	p, t := float64(h.Peak), float64(h.Calc.Cnt)
 	if cumul {
 		p = t
@@ -147,25 +151,31 @@ func (h *Histogram) Graph(width int, limit int, cumul bool, nozero ...bool) {
 	}
 }
 
-func HistTest(n int) {
+func HistTest(sample int) {
 	var h Histogram
+
+	par := func(a, b float64) float64 {
+		const s = 100
+		m, n := int(math.Round(s*a)), int(math.Round(s*b))
+		return float64(h.RNG.Int(m, n)) / s
+	}
+
 	if true {
 		h.Reset()
 		h.Scale(1, 4)
-		ɑ := 3.25
+		ɑ := par(1.5, 3.5)
 		h.Title = fmt.Sprintf("Gamma distribution (ɑ = %v)", ɑ)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.Gamma(ɑ)
 			h.Add(float64(x))
 		}
-		h.Graph(100, 50, false)
+		h.Graph(100, 30, false)
 	}
 	if true {
 		h.Reset()
-		h.Scale(1, 1)
-		ƛ := float64(h.RNG.Int(200, 400)) / 100
+		ƛ := par(2, 4)
 		h.Title = fmt.Sprintf("Poisson distribution (ƛ = %v)", ƛ)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.Poisson(ƛ)
 			h.Add(float64(x))
 		}
@@ -174,32 +184,32 @@ func HistTest(n int) {
 	if true {
 		h.Reset()
 		h.Scale(1, 10)
-		ξ, ω, ɑ := 0., 1., 4.
+		ξ, ω, ɑ := 0., 1., par(-5, 5)
 		h.Title = fmt.Sprintf("Skew-normal distribution (ξ = %v, ω = %v, ɑ = %v)", ξ, ω, ɑ)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.SkewNormal(ξ, ω, ɑ)
 			h.Add(float64(x))
 		}
-		h.Graph(100, 50, false)
+		h.Graph(100, 30, false)
 	}
 	if true {
 		h.Reset(math.Floor)
 		h.Scale(1, 5)
-		ƛ := 2.71
+		ƛ := par(2, 3)
 		h.Title = fmt.Sprintf("Exponential distribution (ƛ = %v)", ƛ)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.Exponential(ƛ)
 			h.Add(float64(x))
 		}
 		h.Graph(100, 100, false)
-		h.Graph(100, 100, true)
+		// h.Graph(100, 100, true)
 	}
 	if true {
 		h.Reset()
-		h.Scale(1, 1)
-		ɑ1, ɑ2 := 0.2, 1.9
+		ɑ1 := par(0.05, 0.15) * 2
+		ɑ2 := (5 - ɑ1) / 2
 		h.Title = fmt.Sprintf("Hermite distribution (ɑ1 = %v, ɑ2  = %v)", ɑ1, ɑ2)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.Hermite(ɑ1, ɑ2)
 			h.Add(float64(x))
 		}
@@ -207,11 +217,42 @@ func HistTest(n int) {
 	}
 	if true {
 		h.Reset()
-		h.Scale(1, 1)
-		p := float64(h.RNG.Int(55, 65)) / 100
+		p := par(0.4, 0.6)
 		h.Title = fmt.Sprintf("Geometric distribution (p = %v)", p)
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			x := h.RNG.Geometric(p)
+			h.Add(float64(x))
+		}
+		h.Graph(100, 100, false)
+	}
+	if true {
+		h.Reset()
+		n := 52
+		p := 0.5
+		h.Title = fmt.Sprintf("Binomial distribution (n = %d, p = %v)", n, p)
+		for h.Calc.Cnt < sample {
+			x := h.RNG.Binomial(n, p)
+			h.Add(float64(x))
+		}
+		h.Graph(100, 100, false)
+	}
+	if true {
+		h.Reset()
+		h.Scale(1, 1)
+		a, b := 0., 20.
+		mode := par(a+1, b-1)
+		h.Title = fmt.Sprintf("Triangular distribution (a = %v, b = %v, mode = %v)", a, b, mode)
+		for h.Calc.Cnt < sample {
+			x := h.RNG.Triangular(a, b, mode)
+			h.Add(float64(x))
+		}
+		h.Graph(100, 100, false)
+	}
+	if false {
+		h.Reset()
+		h.Title = "3 dice throw sum"
+		for h.Calc.Cnt < sample {
+			x := h.RNG.Int(1, 6) + h.RNG.Int(1, 6) + h.RNG.Int(1, 6)
 			h.Add(float64(x))
 		}
 		h.Graph(100, 100, false)
@@ -220,7 +261,7 @@ func HistTest(n int) {
 		h.Reset()
 		h.Scale(1, 1)
 		h.Title = "Sic-Bo"
-		for h.Calc.Cnt < n {
+		for h.Calc.Cnt < sample {
 			d, _, _ := h.RNG.SicBo()
 			h.RNG.Sort(&d)
 			x := 0
